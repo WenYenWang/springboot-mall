f0cbfe62e75c9b5db8f1006ae15d734161ace380
WenYenWang <ksyweedy@gmail.com>
2022-11-17 14:54:31 +0800

    商品功能 - 實做查詢商品列表（基礎實作）

--- src/main/java/com/wywang/springbootmall/dao/ProductDao.java
+++ src/main/java/com/wywang/springbootmall/dao/ProductDao.java
@@ -3,8 +3,11 @@ package com.wywang.springbootmall.dao;
 import com.wywang.springbootmall.dto.ProductRequest;
 import com.wywang.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
 
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- src/main/java/com/wywang/springbootmall/service/ProductService.java
+++ src/main/java/com/wywang/springbootmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.wywang.springbootmall.service;
 import com.wywang.springbootmall.dto.ProductRequest;
 import com.wywang.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
